29729d45f618921ef7f6a91035044d5c4a0aa649
Joshua Nordstrom <nordstromenator@gmail.com>
2014-07-08 13:43:05 -0500

    Automatically Committed for MakeGamesWithUs

---
+++ GameOfLife.spritebuilder/Grid.h
@@ -0,0 +1,18 @@
+//
+//  Grid.h
+//  GameOfLife
+//
+//  Created by Joshua Nordstrom on 7/8/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Grid : CCSprite
+
+@property (nonatomic, assign) int totalAlive;
+@property (nonatomic, assign) int generation;
+
+
+
+@end
