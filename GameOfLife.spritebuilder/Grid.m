fa94b80e9f5854114b0ee63fded56826b52a9d70
Joshua Nordstrom <nordstromenator@gmail.com>
2014-07-08 14:10:43 -0500

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.m
+++ GameOfLife.spritebuilder/Grid.m
@@ -143,6 +143,7 @@ static const int GRID_COLUMNS = 10;
 
 -(void)updateCreatures
 {
+    int numAlive = 0;
     // iterate through the rows
     // note that NSArray has a method 'count' that will return the number of elements in the array
     for (int i = 0; i < [_gridArray count]; i++)
@@ -157,6 +158,7 @@ static const int GRID_COLUMNS = 10;
             if(currentCreature.livingNeighbors == 3)
             {
                 currentCreature.isAlive = TRUE;
+                numAlive += 1;
             }
             else if(currentCreature.livingNeighbors <= 1 || currentCreature.livingNeighbors >= 4 )
             {
@@ -164,6 +166,8 @@ static const int GRID_COLUMNS = 10;
             }
         }
     }
+    
+    _totalAlive = numAlive;
 }
 
 - (BOOL)isIndexValidForX:(int)x andY:(int)y
